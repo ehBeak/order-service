2e20a5474f7d5078480d34e7f4334140dbf73ccc
eunhee <dmsgml9903@gmail.com>
2022-03-30 10:15:29 +0900

    [IMPROVE] Address 클래스(값타입) 불변 객체로 변경

--- src/main/java/com/project/orderservice/domain/Address.java
+++ src/main/java/com/project/orderservice/domain/Address.java
@@ -1,20 +1,35 @@
 package com.project.orderservice.domain;
 
+import lombok.Getter;
+
 import javax.persistence.Embeddable;
 
 @Embeddable
+@Getter
 public class Address {
 
     private String city;
     private String street;
     private int zipcode;
 
-    protected Address() {
+    public Address() { // 기본 생성자 필요
     }
 
     public Address(String city, String street, int zipcode) {
         this.city = city;
         this.street = street;
+        this.zipcode = zipcode;
+    }
+
+    private void setCity(String city) {
+        this.city = city;
+    }
+
+    private void setStreet(String street) {
+        this.street = street;
+    }
+
+    private void setZipcode(int zipcode) {
         this.zipcode = zipcode;
     }
 }
